43a938dd0010a95dcce21fc30405a7d56e141a13
AlianBlank <alianblank@outlook.com>
2023-05-24 18:13:30 +0800

    test

--- jenkins/main.py
+++ jenkins/main.py
@@ -2,12 +2,12 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import os
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    print(f'Hi, {name} os{os.name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    exit(1)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
